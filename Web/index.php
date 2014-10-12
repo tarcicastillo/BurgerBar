c60ef35e42272920fd6ccacfad960e8c04ba266b
charliealbright <albrightc25@gmail.com>
2014-10-12 14:22:29 -0500

    added more file structure to Web portion.

--- Web/index.php
+++ Web/index.php
@@ -1,8 +1,13 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Hello World</title>
+    <meta charset="utf-8">
+    <meta content="" name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <title>Test Page</title>
 </head>
+
 <body>
-	<?php echo '<p>Hello World</p>'; ?> 
+    <p>Hello World!</p>
 </body>
 </html>

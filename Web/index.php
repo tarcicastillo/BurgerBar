0dea3893ab22e137f4c09e072fd046e9bfccdea9
charliealbright <albrightc25@gmail.com>
2014-10-12 14:27:55 -0500

    stuff

---
+++ Web/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta content="" name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <title>Test Page</title>
+</head>
+
+<body>
+    <p>Hello World!</p>
+</body>
+</html>
